13ed55204352bb47dcb5b9ef7da09f4e1ec0dc30
Rebecca Vessal <rebeccaVessal@gmail.com>
2015-09-10 20:24:43 -0700

    Added border to message box.

--- Assets/Scripts/MessageBox.cs
+++ Assets/Scripts/MessageBox.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class MessageBox : MonoBehaviour 
 {
+	public Image borderImage;
 	// Use this for initialization
 	void Start () 
 	{
@@ -12,15 +13,27 @@ public class MessageBox : MonoBehaviour
 		this.gameObject.SetActive(false);	
 	}
 
+	public void Selected()
+	{
+		borderImage.canvasRenderer.SetAlpha(1.0f);
+	}
+
+	public void Deselected()
+	{
+		borderImage.canvasRenderer.SetAlpha(0.0f);
+	}
+
 	void ToggleVisibility()
 	{
 		if(this.gameObject.activeSelf)
 		{
 			this.gameObject.SetActive(false);
+			borderImage.canvasRenderer.SetAlpha(0.0f);
 		}
 		else
 		{
 			this.gameObject.SetActive(true);
+			borderImage.canvasRenderer.SetAlpha(0.0f);
 		}
 	}
 }
